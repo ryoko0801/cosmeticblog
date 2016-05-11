9173fd812d4322eda3d71dd47a3da24b6c2e279e
Ryoko <ryoko.nagata0801@gmail.com>
2016-05-10 22:07:06 -0700

    footer
    menu

---
+++ partials/footer.php
@@ -0,0 +1,78 @@
+       <section class="footer-back">
+       	<div class="row">
+       		<div class="large-6 columns">
+       			<div class="foot-nav">
+       				<ul>
+       					<li><img src="images/logo.png" alt="logo"></li>
+       					<li><p>570 Dunsmuir Street, Suite 600 Vancouver, BC,<br>Canada V6B 1Y1</p></li>
+       					<li><p>Phone: 604-682-ARTS (2787)</p></li>
+       					<li><p>Email: info@vanarts.com</p></li>
+       				</ul>
+       			</div>
+       		</div>
+       		<div class="medium-6 large-6 columns">
+
+       			<div class="foot-bar right">
+       				<ul class="">
+       					<ul class="foot-nav-item">
+       						<li class="left"><a href="#">HOME</a></li>
+       						<li class="left"><a href="event.php">EVENT</a></li>
+       						<li class="left"><a href="gallery.php">GALLERY</a></li>
+       						<li class="left"><a href="index.php#contact">CONTACT</a></li>
+       					</ul>
+       				</ul>
+
+       				<ul class="right nav-icon">
+       					<li class="left">
+       						<a href='#'><i class="fa fa-facebook-square fa-3x white"></i></a>
+       					</li>
+       					<li class="left">
+       						<a href='#'><i class="fa fa-twitter-square fa-3x white"></i></a>
+       					</li>
+       					<li class="left">
+       						<a href='#'><i class="fa fa-google-plus-square fa-3x white"></i></a>
+       					</li>
+       					<li class="left">
+       						<a href='#'><i class="fa fa-instagram fa-3x white"></i></a>
+       					</li>
+       				</ul>
+       			</div>
+       		</div>
+       	</div>
+
+
+       </section>
+
+
+
+
+
+
+
+
+       <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+       <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.3.min.js"><\/script>')</script>
+
+
+       <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+   </body>
+   </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+   <script src="js/vendor/jquery.js"></script>
+   <script src="js/vendor/what-input.js"></script>
+   <script src="js/vendor/foundation.js"></script>
+   <script src="js/app.js"></script>
+</body>
+</html>
---
+++ partials/header.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Foundation for Sites</title>
+  <link rel="stylesheet" href="css/foundation.css">
+  <link rel="stylesheet" href="css/style.css">
+  <!-- google font -->
+  <link href='https://fonts.googleapis.com/css?family=Josefin+Slab:400,600,700|Prata|Limelight' rel='stylesheet' type='text/css'>
+  <!-- font awesome -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+</head>
+<body>
+
+  <!-- navigation -->
+  <div class ="row">
+    <div class="large-offset-9 large-3 columns">
+     <ul class="right nav-icon">
+      <li class="left">
+        <a href='#'><i class="fa fa-facebook-square fa-3x  black"></i></a>
+      </li>
+      <li class="left">
+        <a href='#'><i class="fa fa-twitter-square fa-3x  black"></i></a>
+      </li>
+      <li class="left">
+        <a href='#'><i class="fa fa-google-plus-square fa-3x  black"></i></a>
+      </li>
+      <li class="left">
+        <a href='#'><i class="fa fa-instagram fa-3x  black"></i></a>
+      </li>
+    </ul>
+  </div>
+</div>
+
+<div class ="row">
+  <div class="large-12 medium-12 columns ">
+    <div class="title-bar" data-responsive-toggle="main-nav" data-hide-for="medium">
+      <button class="menu-icon" type="button" data-toggle></button>
+      <div class="title-bar-title">Menu</div>
+    </div>
+    <div id="main-nav" class ="top-nav">
+     <div class ="bottom-line">
+      <ul class="">
+      <li><a href="#">What’s New</a></li>
+        <li><a href="reviews.php">Reviews</a></li>    
+        <li><a href="trend.php">Trend</a></li>         
+        <li><a href="about.php">About</a></li>         
+        <li><a href="contact.php">Contact</a></li>
+      </ul>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- nd of nav -->   
